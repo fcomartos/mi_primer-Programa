3633b9822813afb761d7b23c4b7ae7f1a0027e01
fcomartos <fcomartos1@outlook.es>
2019-04-13 20:02:50 +0200

    Añadiendo calculadora grados

---
+++ calculadora_grados.py
@@ -0,0 +1,7 @@
+
+
+farenheit = float(input("Escribe los grados Farenheit: "))
+
+celsius = int((farenheit - 32)/ 1.8)
+
+print("{} grados Farenheit son {} grados Celsius".format(farenheit, celsius))
