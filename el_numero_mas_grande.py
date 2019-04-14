34f1725a43af83b5afb9e4a4e26b85659038b827
fcomartos <fcomartos1@outlook.es>
2019-04-14 19:49:12 +0200

    Añadiendo el número más grande

---
+++ el_numero_mas_grande.py
@@ -0,0 +1,21 @@
+
+numeros_usuario = []
+numero_del_usuario = ""
+
+while len(numeros_usuario) < 10:
+    while not numero_del_usuario.isdigit():
+
+        numero_del_usuario = input("Dime un numero: ")
+    numeros_usuario.append(int(numero_del_usuario))
+    numero_del_usuario = ""
+    print("Numero añadido")
+
+print(numeros_usuario)
+
+numero_grande = numeros_usuario[0]
+
+for numero in numeros_usuario:
+    if numero > numero_grande:
+        numero_grande = numero
+
+print("El numero más grande es {} ".format(numero_grande))
